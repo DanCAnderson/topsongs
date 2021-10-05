caf6db6b0bdc0f98cf75cf8f96c426ea584a8db0
Dan A <andersondanc@gmail.com>
2021-10-05 12:07:15 -0500

    Added pagination to song list, via react-pagination pacakge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SongTable from './components/songTable';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SongTable />
   </React.StrictMode>,
   document.getElementById('root')
 );
