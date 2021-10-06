7a9deb7bcd540bbe406a8fd23ee20729ff376ee0
Dan A <andersondanc@gmail.com>
2021-10-05 20:49:20 -0500

    Added groundwork for table sorting

--- src/components/songTable.js
+++ src/components/songTable.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
 import ReactPaginate from 'react-paginate';
 
-const songs = require('../topSongs.json');
+let songs = require('../topSongs.json');
 
 class SongTable extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            data: songs.slice(0,10),
+            display: songs.slice(0,10),
             pageCount: Math.ceil(songs.length / 10)
         };
     }
 
     handlePageClick = (data) => {
-        this.setState({data: songs.slice(data.selected * 10, data.selected * 10 + 10)});
+        this.setState({display: songs.slice(data.selected * 10, data.selected * 10 + 10)});
+    }
+
+    sortTable = (column, reverse=false) => {
+
+        const order = (bool) => {
+            return reverse ? !bool : bool;
+        }
+
+        songs.sort( (a, b) => {
+            let compare;
+
+            switch (column) {
+                case '2004':
+                    compare = a.ranking[2004] < b.ranking[2004];
+                    break;
+                case '2021':
+                    compare = a.ranking[2021] < b.ranking[2021];
+                    break;
+                case 'title':
+                    compare = a.title > b.title;
+                    break;
+                case 'artist':
+                    compare = a.artist > b.artist;
+                    break;
+                default:
+                    compare = 0;
+            }
+
+            return order(compare);
+        });
     }
 
     render() {
         return (
             <div>
                 <ul>
-                    {this.state.data.map((song, index) => {
-                        return <li key={index}>{song.ranking[2004]}. {song.title}, {song.artist}. {song.year}</li>
+                    {this.state.display.map((song, index) => {
+                        return <li key={index}>{song.ranking[2004]} / {song.ranking[2021]} {song.title}, {song.artist}. {song.year}</li>
                     })}
                 </ul>
                 <ReactPaginate
@@ -31,6 +61,7 @@ class SongTable extends Component {
                     pageRangeDisplayed={5}
                     onPageChange={this.handlePageClick}
                 />
+                <button onClick={this.sortTable('title')}>Sort</button>
             </div>
         );
     }
